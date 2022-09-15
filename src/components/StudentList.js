a5696c944dbd111ecf3035cbf9831470e6096a9b
SahilPatel146 <20it146@charusat.edu.in>
2022-09-15 21:48:23 +0530

    Implementing Student Form For Save

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Table } from 'react-bootstrap'
+import { Card, Container, Table } from "react-bootstrap";
 
 export default function StudentList() {
     return (
@@ -31,6 +32,36 @@ export default function StudentList() {
                         </tr>
                     </tbody>
                 </Table>
+            </Container>
+            <Container>
+                <Card.Header><h3>Students List</h3></Card.Header>
+                <Card.Body>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Student Id</th>
+                                <th>Student Name</th>
+                                <th>Student Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td colSpan={2}>Larry the Bird</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Card.Body>
             </Container>
         </div>
     )
